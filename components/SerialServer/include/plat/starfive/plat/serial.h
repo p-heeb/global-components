2cb7c99e937a3600b735a584ae7190a20c636c61
Petra Heeb <petra.heeb1@ibm.com>
2023-10-06 14:05:44 +0200

    update serialserver component with new starfive platform
    Signed-off-by: Petra Heeb <petra.heeb1@ibm.com>

---
+++ components/SerialServer/include/plat/starfive/plat/serial.h
@@ -0,0 +1,21 @@
+/*
+ * Copyright 2019, DornerWorks
+ * Copyright 2019, Data61, CSIRO (ABN 41 687 119 230)
+ * Coptright 2023, IBM
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+#pragma once
+
+#define HARDWARE_SERIAL_INTERFACES  \
+    emits Dummy dummy_source;       \
+    consumes Dummy serial_dev;
+
+#define HARDWARE_SERIAL_ATTRIBUTES
+
+#define HARDWARE_SERIAL_COMPOSITION                                                 \
+        connection seL4DTBHardware serial_conn(from dummy_source, to serial_dev);
+
+#define HARDWARE_SERIAL_CONFIG                               \
+        serial_dev.dtb = dtb({ "chosen" : "stdout-path" });  \
+        serial_dev.generate_interrupts = 1;
