4789fbe22ea498779df8ca0c270162d67bbebedc
Petra Heeb <petra.heeb1@ibm.com>
2023-10-06 14:06:14 +0200

    update timeserver component with new starfive platform
    Signed-off-by: Petra Heeb <petra.heeb1@ibm.com>

---
+++ components/TimeServer/include/plat/starfive/plat/timers.h
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2019, DornerWorks
+ * Copyright 2019, Data61, CSIRO (ABN 41 687 119 230)
+ * Coptright 2023, IBM
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+#pragma once
+
+#define HARDWARE_TIMER_INTERFACES                                                   \
+    consumes Dummy pwm0_timer;                                                      \
+    consumes Dummy pwm1_timer;                                                      \
+    emits Dummy dummy_source;
+
+#define HARDWARE_TIMER_ATTRIBUTES
+
+#define HARDWARE_TIMER_COMPOSITION                                                        \
+        connection seL4DTBHardware pwm_timer_conn0(from dummy_source, to pwm0_timer);     \
+        connection seL4DTBHardware pwm_timer_conn1(from dummy_source, to pwm1_timer);
+
+#define HARDWARE_TIMER_CONFIG                                                             \
+        pwm0_timer.dtb = dtb({"path" : "/soc/pwm@120d0000"});                             \
+        pwm0_timer.generate_interrupts = 1;                                               \
+        pwm1_timer.dtb = dtb({"path" : "/soc/pwm@120d1000"});                             \
+        pwm1_timer.generate_interrupts = 1;
+
+#define HARDWARE_TIMER_PLAT_INTERFACES
